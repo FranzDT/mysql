0dbf7878966edb5b80deff5c73ad3f9fb00348da
Franz Vincent Dela Torre <52233410+FranzDT@users.noreply.github.com>
2019-07-18 08:48:15 +0800

    Create exercise1_1.sql

---
+++ basic/exercise1_1.sql
@@ -0,0 +1,13 @@
+    
+CREATE TABLE persons (
+	ID INT(11) NOT NULL AUTO_INCREMENT,
+	first_name VARCHAR(45),
+	last_name VARCHAR(45),
+	age INT(2),
+	gender ENUM ('M','F'),
+	address VARCHAR(45),
+	PRIMARY KEY (ID)
+);
+
+CREATE INDEX PRIMARY_KEY
+ON persons (ID);
