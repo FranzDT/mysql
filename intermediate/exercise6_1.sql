287931e17a348d4e859d5d6f6fc5d63fab8a94f5
Franz Vincent Dela Torre <52233410+FranzDT@users.noreply.github.com>
2019-07-18 14:54:04 +0800

    Create exercise6_1.sql

---
+++ intermediate/exercise6_1.sql
@@ -0,0 +1,16 @@
+select e.employeeNumber 'D', concat(e.firstName,' ',e.lastName) as 'Employee',
+	CASE
+		WHEN count(c.salesRepEmployeeNumber) > 0 then CASE
+						WHEN count(c.salesRepEmployeeNumber) > 5
+						THEN e.salary * .3
+						WHEN count(c.salesRepEmployeeNumber) < 6
+						THEN e.salary * 0.1
+					END
+		ELSE 0
+	END as 'Bonus'
+FROM employees e
+left join customers c
+on c.salesRepEmployeeNumber = e.employeeNumber
+Where e.jobTitle = 'Sales Rep'
+group by e.employeeNumber
+order by Bonus Desc;
