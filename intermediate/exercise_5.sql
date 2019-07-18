e427fc02a23c72d91824422bae31fb723d05684c
Franz Vincent Dela Torre <52233410+FranzDT@users.noreply.github.com>
2019-07-18 14:44:58 +0800

    Update exercise_5.sql

--- intermediate/exercise_5.sql
+++ intermediate/exercise_5.sql
@@ -1,13 +1,13 @@
-select concat(city,' office') as 'Name', concat(addressLine1,', ',
-	IFNULL(addressLine2,''),', ',
-	IFNULL(state,''),', ',
-	country,', ',
+select concat(city,' office') as 'Name', concat_ws(', ',addressLine1,
+	addressLine2,
+	state,
+	country,
 	postalCode) as 'Address'
 FROM offices
 UNION ALL
-select customerName as 'Name', concat(addressLine1,', ',
-	IFNULL(addressLine2,''),', ',
-	IFNULL(state,''),', ',
-	country,', ',
-	IFNULL(postalCode,'')) as 'Address'
+select customerName as 'Name', concat_ws(', ',addressLine1,
+	addressLine2,
+	state,
+	country,
+	postalCode) as 'Address'
 from customers;
